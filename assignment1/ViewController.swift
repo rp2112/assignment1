1fde2c66c7dafb4457336a8fe73e78f514243b98
Ritu1910 <ritugpatel12@gmail.com>
2024-01-24 16:42:28 -0500

    ass1

--- assignment1/ViewController.swift
+++ assignment1/ViewController.swift
@@ -13,7 +13,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var disnum = 0
+    var increvalue = 1
+    @IBOutlet weak var output: UILabel!
+    
+    @IBAction func btndecre(_ sender: Any) {
+        disnum -= increvalue
+        output.text = String(disnum)
+    }
+    
+    @IBAction func btnplus(_ sender: Any) {
+        disnum +=  increvalue
+        output.text = String(disnum)
+    }
+    
+    @IBAction func btnreset(_ sender: Any) {
+        disnum = 0
+        increvalue = 1
+        output.text = String(0)
+    }
+    
+    @IBAction func btnstep2(_ sender: Any) {
+        increvalue = 2
+    }
 }
 
